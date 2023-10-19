341b3be834fd7f7e6284874cd1016337e0300243
kimyonghui <kyhui1115@gmail.com>
2023-10-19 16:20:03 +0900

    [design] 화면 높이 설정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,9 @@
 import Logo from './Logo';
-import SearchBar from './SearchBar';
 
 function Header() {
   return (
-    <div className="relative flex items-center justify-center w-screen h-20 bg-slate-400">
+    <div className="absolute top-0 flex items-center justify-center w-screen h-20 bg-slate-400">
       <Logo />
-      <SearchBar />
     </div>
   );
 }
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,11 @@
+import SearchSection from './searchSection/SearchSection';
+
+function Main() {
+  return (
+    <div className="flex h-screen pt-20">
+      <SearchSection />
+    </div>
+  );
+}
+
+export default Main;
