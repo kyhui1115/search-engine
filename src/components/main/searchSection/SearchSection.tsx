a440075d178399746e7d3742cce702f760d63419
kimyonghui <kyhui1115@gmail.com>
2023-10-21 17:10:11 +0900

    [refactor] ResizingBar 컴포넌트 생성 및 역할분리

--- src/components/main/searchSection/SearchSection.tsx
+++ src/components/main/searchSection/SearchSection.tsx
@@ -1,61 +1,32 @@
+import { useState } from 'react';
 import SearchBar from 'components/header/SearchBar';
 import SearchList from './SearchList';
 import SearchInfo from './SearchInfo';
-import { useEffect, useState } from 'react';
+import Pagination from './Pagination';
+import ResizingBar from './ResizingBar';
 
 function SearchSection() {
   const [isResizing, setIsResizing] = useState(false);
   const [initialX, setInitialX] = useState(0);
   const [width, setWidth] = useState(500);
 
-  const handleMouseDown = (e: React.MouseEvent) => {
-    e.preventDefault();
-    setIsResizing(true);
-    setInitialX(e.clientX);
-  };
-
-  const handleMouseUp = () => {
-    setIsResizing(false);
-  };
-
-  const handleMouseMove = (e: MouseEvent) => {
-    if (isResizing) {
-      const newWidth = width + e.clientX - initialX;
-      setInitialX(e.clientX);
-      if (newWidth >= 430) {
-        setWidth(newWidth);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (isResizing) {
-      document.addEventListener('mousemove', handleMouseMove);
-      document.addEventListener('mouseup', handleMouseUp);
-    } else {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-    }
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-    };
-  }, [isResizing]);
-
   return (
-    <div className="flex">
-      <div className="flex flex-col" style={{ width }}>
+    <>
+      <div className="flex flex-col items-center" style={{ width }}>
         <SearchBar />
         <SearchInfo />
         <SearchList />
+        <Pagination />
       </div>
-      <div
-        className="w-1.5 bg-blue-300 cursor-col-resize hover:bg-blue-500"
-        onMouseDown={handleMouseDown}
+      <ResizingBar
+        isResizing={isResizing}
+        setIsResizing={setIsResizing}
+        initialX={initialX}
+        setInitialX={setInitialX}
+        width={width}
+        setWidth={setWidth}
       />
-      <div className="flex items-center justify-center w-[1000px]">right</div>
-    </div>
+    </>
   );
 }
 
