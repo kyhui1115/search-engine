ac4165822adda5b27c5645d012e70bd167933dd8
kimyonghui <kyhui1115@gmail.com>
2024-01-12 15:22:51 +0900

    feat: 페이지네이션 기능 추가

--- src/components/main/searchSection/Pagination.tsx
+++ src/components/main/searchSection/Pagination.tsx
@@ -1,24 +1,52 @@
+import { useState, type Dispatch, type SetStateAction } from 'react';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
-export default function Pagination() {
+interface pagination {
+  currentPage: number;
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+}
+
+export default function Pagination({
+  currentPage,
+  setCurrentPage
+}: pagination) {
+  const [pageList, setPageList] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+  const leftArrowHandler = () => {
+    if (currentPage < 11) return;
+    setPageList((prev) => prev.map((page) => page - 10));
+    setCurrentPage((prev) => prev - 10);
+  };
+  const rightArrowHandler = () => {
+    setPageList((prev) => prev.map((page) => page + 10));
+    setCurrentPage((prev) => prev + 10);
+  };
+
   return (
-    <div className="flex items-center justify-between w-full px-8 h-14 bg-slate-100">
-      <MdKeyboardArrowLeft className="w-6 h-6 cursor-pointer hover:text-blue-300" />
-      <input
-        type="button"
-        value="1"
-        className="font-bold text-blue-500 cursor-pointer"
-      />
-      <div className="cursor-pointer hover:text-blue-300">2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-      <div>10</div>
-      <MdKeyboardArrowRight className="w-6 h-6" />
+    <div className="flex items-center justify-between w-full h-12 px-8 border-y bg-slate-100 border-slate-400">
+      <label>
+        <input type="button" className="hidden" onClick={leftArrowHandler} />
+        <MdKeyboardArrowLeft className="w-6 h-6 cursor-pointer hover:text-blue-300" />
+      </label>
+      {pageList.map((page) => (
+        <input
+          key={page}
+          type="button"
+          value={page}
+          onClick={() => {
+            setCurrentPage(page);
+          }}
+          className={`cursor-pointer 0 ${
+            currentPage === page
+              ? 'font-bold text-blue-500'
+              : 'hover:text-blue-30'
+          }`}
+        />
+      ))}
+      <label>
+        <input type="button" className="hidden" onClick={rightArrowHandler} />
+        <MdKeyboardArrowRight className="w-6 h-6 cursor-pointer hover:text-blue-300" />
+      </label>
     </div>
   );
 }
