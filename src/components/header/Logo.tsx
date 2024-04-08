87f9d71cd7b4a5d7ae644b3f6b22fc84d30ab0db
kimyonghui <kyhui1115@gmail.com>
2024-04-08 10:23:56 +0900

    design: 로고 너비 수정

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,6 +1,6 @@
 function Logo() {
   return (
-    <div className="flex flex-col items-center justify-center w-48">
+    <div className="flex flex-col items-center justify-center w-52">
       <span className="text-3xl font-bold tracking-wider text-slate-200">
         Spider Web
       </span>
